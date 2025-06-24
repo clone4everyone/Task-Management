7c8d09f2514a66fb4e6c25c1f8780bf538f413b9
clone4everyone <karanrawat9149@gmail.com>
2025-06-24 23:29:14 +0530

    fixes bug related to cors

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://task-management-seven-eta.vercel.app'
+}));
+
 app.use(express.json());
 
 // Routes
